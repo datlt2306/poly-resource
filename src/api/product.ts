9869319d88ac7efcca98085091d538357d83d71f
Le Trong Dat <datlt2306@gmail.com>
2022-12-09 08:14:25 +0700

    init

--- src/api/product.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosPromise } from 'axios';
-import { IProduct } from '../interfaces/product';
-import instance from './instance';
-
-export const getProducts = async (): Promise<IProduct[]> => {
-    return await instance.get('/products');
-}
